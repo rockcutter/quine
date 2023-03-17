1b92287a8d55779f205320ed79509b07bbe50662
rockcutter <rockcutter414@gmail.com>
2023-03-17 15:18:08 +0900

    [change] 上の棒と下の棒を追加

--- rockcutter.cpp
+++ rockcutter.cpp
@@ -5,75 +5,47 @@
 
 using namespace std::string_literals;
 
-std::string encoded = R"(                                     #in                                                                                                                             
-                                                              clu                                                                                                                             
-                                                              de$                                                                                                                             
-                                                              SPA                                                                                                                             
-                                                              CE<                                                                  i             o                                            
-                                                              str                                                                eam           >$N                                            
-                                                              EWL                                                                INE           #in                                            
-                                                              clu                                                                de$           SPA                                            
-                                                              CE<                                                                str           ing                                            
-                                                              >$N                                                                EWL           INE                                            
-#inc    lud          e$SPAC                 E<strin           g_v        iew>          $NEWLIN          E#in          clud    e$SPACE<rege  x>$NEWLINE$N         EWLINEu           sing    $SP
-ACEn  amesp       ace$SPACEstd            ::string_li         ter       als;         $NEWLINE$NE        WLIN          Estd    ::string$SPA  CEencoded$SP       ACE=$SPACE{         }{}{  };$NE
-WLIN E$NEWL      INEstd::string          $SPACERemoveSp       ace      (con         st$SPACEstd::s      trin          g&$S    PACEstr){$NE  WLINE$TABret      urn$SPACEstd:        :reg ex_rep
-lace (str,      $SPACE    std::r        egex("    $SPAC       E")     ,$SP         ACE"")    ;$NEW      LINE          }$NE       WLI           NE$           NEWLI     NEstd       ::st ring$ 
-SPACERem       oveNe        wLine      (cons        t$SP      ACE    std:         :stri        ng&$     SPAC          Estr       ){$           NEW          LINE$       TABre      turn$SPA   
-CEstd:         :reg          ex_r      epla          c        e(s   tr,$          SPAC          E       std:          :reg       ex(           "\n          "),$         SPAC      E"");$     
-NEWLI         NE}$            NEWL    INE$                    NEW  LINE          std:                   :str          ing$       SPA           CER         esto           reS      tring      
-(std:         :str            ing$    SPAC                    Est  r){           $NEW                   LINE          $TAB       sta           tic         $SPA           CEco     nst$S      
-PACE          std:            :str    ing$                    SPA CESP           ACE_                   TOKE          N$TA       B$T           AB=         $SPACE"\\$"s$SPACE+     $SPA       
-CE"S          PACE            ";$N    EWLI                    NE$TABst           atic                   $SPA          CEco       nst           $SP         ACEstd::string$SPAC     ETAB       
-_TOK          EN$T            AB$T    AB$S                    PACE$SPAC          E=$S                   PACE          "\\$       "s$           SPA         CE+$SPACE"TAB";$NEW     LINE       
-$TAB          stat            ic$S    PACE                    cons  t$SP         ACEs                   td::          stri       ng$           SPA         CENE                    WLIN       
-E_TO          KEN$            TAB$    SPAC                    E$S    PACE        =$SP                   ACE"          \\$"       s$S           PAC         E+$S                    PACE       
-"NEW          LINE            ";$N    EWLI                    NE$    TABs        tati                   c$SP         ACEco       nst           $SP         ACEs                    td::       
-stri           ng$S          PACE      SPAC          E_       CHA     RACT        ER$T          AB      =$SP         ACE"$       SPA           CE"          ;$NE                   WLIN       
-E$TA           Bstat        ic$SP      ACEco        nst$      SPA      CEst       d::st        ring      $SPA       CETAB_       CHAR          ACTE         RS$TA         B$S      PACE       
-=$SP            ACE"$T    AB";$N        EWLINE     $TABs      tat       ic$S       PACEco     nst$S      PACEs    td::stri       ng$S          PACE          NEWLIN     E_CHA      RACT       
-ERS$             SPACE=$SPACE"\          n";$NEWLINE$TA       Bst       r$SP        ACE=$SPACEstd:        :regex_repl ace(       str,$SPAC     Estd::reg      ex(SPACE_TOKEN)      ,$SP       
-ACES              PACE_CHARACT            ER);$NEWLINE        $TA        Bstr        $SPACE=$SPAC         Estd::rege  x_re        place(st      r,$SPACE       std::regex(T        AB_T       
-OKEN                ),$SPACE                TAB_CHAR          ACT         ERS)         ;$NEWLIN             E$TABs    tr$S         PACE=$        SPACEs          td::rege          x_re       
-place(str,$SPACEstd::regex(NEWLINE_TOKEN),$SPACENEWLINE_CHARACTERS);$NEWLINE$TABreturn$SPACEstr;$NEWLINE}$NEWLINE$NEWLINEstd::string$SPACEMiniFormat(std::string_view$SPACEcontent,$SPACEstd::string$SPACEarg,$SPACEsize_t$SPACEindex$SPACE=$SPACE0){$NEWLINE$TABauto$SPACEformatPoint$SPACE=$SPACEcontent.find("{}");$NEWLINE$TABfor(size_t$SPACEi$SPACE=$SPACE0;$SPACEi$SPACE<$SPACEindex;$SPACE++i){$NEWLINE$TAB$TABformatPoint$SPACE=$SPACEcontent.find("{}",$SPACEformatPoint$SPACE+$SPACE1);$NEWLINE$TAB}$NEWLINE$TAB$NEWLINE$TABauto$SPACEformer$SPACE=$SPACEcontent.substr(0,$SPACEformatPoint);$NEWLINE$TABauto$SPACElatter$SPACE=$SPACEcontent.substr(formatPoint$SPACE+$SPACE2);$NEWLINE$TABreturn$SPACEstd::string{former}$SPACE+$SPACEarg$SPACE+$SPACEstd::string{latter};$NEWLINE}$NEWLINE$NEWLINEint$SPACEmain(){$NEWLINE$TABauto$SPACEdecoded$SPACE=$SPACERestoreString(RemoveSpace(RemoveNewLine(encoded)));$NEWLINE$TABstd::string$SPACEoutString;$NEWLINE$TABoutString$SPACE=$SPACEMiniFormat(decoded,$SPACE"R\"(");$NEWLINE$TABoutString$SPACE=$SPACEMiniFormat(outString,$SPACE")\"",$SPACE1);$NEWLINE$TABoutString$SPACE=$SPACEMiniFormat(outString,$SPACEencoded);$NEWLINE$TABstd::cout$SPACE<<$SPACEoutString;$NEWLINE}$NEWLINE)";
-
-std::string RemoveSpace(const std::string& str){
-    return std::regex_replace(str, std::regex(" "), "");
-}
-
-std::string RemoveNewLine(const std::string& str){
-    return std::regex_replace(str, std::regex("\n"), "");
-}
-
-std::string RestoreString(std::string str){
-    static const std::string SPACE_TOKEN        = "\\$"s + "SPACE";
-    static const std::string TAB_TOKEN          = "\\$"s + "TAB";
-    static const std::string NEWLINE_TOKEN      = "\\$"s + "NEWLINE";
-    static const std::string SPACE_CHARACTER    = " ";
-    static const std::string TAB_CHARACTERS     = "    ";
-    static const std::string NEWLINE_CHARACTERS = "\n";
-    str = std::regex_replace(str, std::regex(SPACE_TOKEN), SPACE_CHARACTER);
-    str = std::regex_replace(str, std::regex(TAB_TOKEN), TAB_CHARACTERS);
-    str = std::regex_replace(str, std::regex(NEWLINE_TOKEN), NEWLINE_CHARACTERS);
-    return str;
-}
-
-std::string MiniFormat(std::string_view content, std::string arg, size_t index = 0){
-    auto formatPoint = content.find("{}");
-    for(size_t i = 0; i < index; ++i){
-        formatPoint = content.find("{}", formatPoint + 1);
-    }
-    
-    auto former = content.substr(0, formatPoint);
-    auto latter = content.substr(formatPoint + 2);
-    return std::string{former} + arg + std::string{latter};
-}
-
-int main(){
-    auto decoded = RestoreString(RemoveSpace(RemoveNewLine(encoded)));
-    std::string outString;
-    outString = MiniFormat(decoded, "R\"(");
-    outString = MiniFormat(outString, ")\"", 1);
-    outString = MiniFormat(outString, encoded);
-    std::cout << outString;
-}
+std::string encoded = R"(
+#include$SPACE<iostream>$NEWLINE#include$SPACE<string>$NEWLINE#include$SPACE<string_view>$NEWLINE#include$SPACE<regex>$NEWLINE$NEWLINEusing$SPACEnamespace$SPACEstd::string_literals;$NEWLINE$
+NEWLINEstd::string$SPACEencoded$SPACE=$SPACE{}{}{};$NEWLINEstd::string$SPACERemoveSpace(const$SPACEstd::string&$SPACEstr){return$SPACEstd::regex_replace(str,$SPACEstd::regex("$SPACE"),$SPACE
+"");}$SPACEstd::string$SPACERemoveNewLine(const$SPACEstd::string&$SPACEstr){return$SPACEstd::regex_replace(str,$SPACE$NEWLINEstd::regex("\n"),$SPACE"");}std::string$SPACERestoreString(std::s
+tring$SPACEstr){static$SPACEconst$SPACEstd::string$SPACESPACE_TOKEN$SPACE=$SPACE"\\$"s$SPACE+$SPACE"SPACE";$SPACEstatic$SPACEconst$SPACEstd::string$SPACETAB_TOKEN$SPACE=$SPACE"\\$"s$SPACE+$S
+                                                              PAC                                                                                                                             
+                                                              E"T                                                                                                                             
+                                                              AB"                                                                                                                             
+                                                              ;$S                                                                                                                             
+                                                              PAC                                                                  E             $                                            
+                                                              NEW                                                                LIN           Est                                            
+                                                              ati                                                                c$S           PAC                                            
+                                                              Eco                                                                nst           $SP                                            
+                                                              ACE                                                                std           ::s                                            
+                                                              tri                                                                ng$           SPA                                            
+CENE    WLI          NE_TOK                 EN$SPAC           E=$        SPAC          E"\\$"s          $SPA          CE+$    SPACE"NEWLIN  E";static$SP         ACEcons           t$SP    ACE
+std:  :stri       ng$SPACESPAC            E_CHARACTER         $SP       ACE=         $SPACE"$SPA        CE";          stat    ic$SPACEcons  t$SPACEstd::       string$SPAC         ETAB  _CHAR
+ACTE RS$SPA      CE=$SPACE"$TAB          ";$SPACE$NEWLI       NEs      tati         c$SPACEconst$S      PACE          std:    :string$SPAC  ENEWLINE_CHA      RACTERS$SPACE        =$SP ACE"\n
+";$S PACEs      tr$SPA    CE=$SP        ACEstd    ::reg       ex_     repl         ace(st    r,$SP      ACEs          td::       reg           ex(           SPACE     _TOKE       N),$ SPACE 
+SPACE_CH       ARACT        ER);s      tr$SP        ACE=      $SP    ACEs         td::r        egex     _rep          lace       (st           r,$          SPACE       std::      regex(TA   
+B_TOKE         N),$          NEWL      INE$          S        PAC   ETAB          _CHA          R       ACTE          RS);       $SP           ACE          str$         SPAC      E=$SPA     
+CEstd         ::re            gex_    repl                    ace  (str          ,$SP                   ACEs          td::       reg           ex(         NEWL           INE      _TOKE      
+N),$S         PACE            NEWL    INE_                    CHA  RAC           TERS                   );re          turn       $SP           ACE         str;           }$SP     ACEst      
+d::s          trin            g$SP    ACEM                    ini Form           at(s                   td::          stri       ng_           vie         w$SPACEcontent,$SPA     CEst       
+d::s          trin            g$SP    ACEa                    rg,$SPAC           Esiz                   e_t$          SPAC       Ein           dex         $SPACE=$SPACE0)$NEW     LINE       
+$SPA          CE{a            uto$    SPAC                    EformatPo          int$                   SPAC          E=$S       PAC           Eco         ntent.find("{}");fo     r(si       
+ze_t          $SPA            CEi$    SPAC                    E=$S  PACE         0;$S                   PACE          i$SP       ACE           <$S         PACE                    inde       
+x;$S          PACE            ++i)    {for                    mat    Poin        t$SP                   ACE=          $SPA       CEc           ont         ent.                    find       
+("{}          ",$S            PACE    form                    atP    oint        $SPA                   CE+$         SPACE       1);           }au         to$S                    PACE       
+form           er$S          PACE      =$SP          AC       Eco     nten        t.su          bs      tr(0         ,$SPA       CEf           orm          atPo                   int)       
+;aut           o$SPA        CElat      ter$S        PACE      =$S      PACE       conte        nt$N      EWLI       NE$SPA       CE.s          ubst         r(for         mat      Poin       
+t$SP            ACE+$S    PACE2)        ;retur     n$SPA      CEs       td::       string     {form      er}$S    PACE+$SP       ACEa          rg$S          PACE+$     SPACE      std:       
+:str             ing{latter};}i          nt$SPACEmain()       {au       to$S        PACEdecoded$SP        ACE=$SPACER esto       reString(     RemoveSpa      ce(RemoveNewLin      e(en       
+code              d)));std::st            ring$SPACEou        tSt        ring        ;outString$S         PACE=$SPAC  E$NE        WLINE$SP      ACEMiniF       ormat(decode        d,$S       
+PACE                "R\"(");                outStrin          g$S         PACE         =$SPACEM             iniFor    mat(         outStr        ing,$S          PACE")\"          ",$S       
+PACE1);outString$SPACE=$SPACEMiniFormat(outString,$SPACEencoded);std::cout$SPACE<<$SPACEoutString;}$NEWLINE)";
+std::string RemoveSpace(const std::string& str){return std::regex_replace(str, std::regex(" "), "");} std::string RemoveNewLine(const std::string& str){return std::regex_replace(str, 
+std::regex("\n"), "");}std::string RestoreString(std::string str){static const std::string SPACE_TOKEN = "\\$"s + "SPACE"; static const std::string TAB_TOKEN = "\\$"s + "TAB"; 
+static const std::string NEWLINE_TOKEN = "\\$"s + "NEWLINE";static const std::string SPACE_CHARACTER = " ";static const std::string TAB_CHARACTERS = "    "; 
+static const std::string NEWLINE_CHARACTERS = "\n"; str = std::regex_replace(str, std::regex(SPACE_TOKEN), SPACE_CHARACTER);str = std::regex_replace(str, std::regex(TAB_TOKEN),
+ TAB_CHARACTERS); str = std::regex_replace(str, std::regex(NEWLINE_TOKEN), NEWLINE_CHARACTERS);return str;} std::string MiniFormat(std::string_view content, std::string arg, size_t index = 0)
+ {auto formatPoint = content.find("{}");for(size_t i = 0; i < index; ++i){formatPoint = content.find("{}", formatPoint + 1);}auto former = content.substr(0, formatPoint);auto latter = content
+ .substr(formatPoint + 2);return std::string{former} + arg + std::string{latter};}int main(){auto decoded = RestoreString(RemoveSpace(RemoveNewLine(encoded)));std::string outString;outString = 
+ MiniFormat(decoded, "R\"(");outString = MiniFormat(outString, ")\"", 1);outString = MiniFormat(outString, encoded);std::cout << outString;}
